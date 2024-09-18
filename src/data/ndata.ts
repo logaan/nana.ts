e41039b5f6d32a4d14e27d39d8e0fb1925ac8e0f
Logan Campbell <colin@logaan.net>
2024-09-19 00:58:09 +1000

    design notes

--- src/data/ndata.ts
+++ src/data/ndata.ts
@@ -1,6 +1,9 @@
 import { Environment } from "./environment";
 import { Process } from "../process/process";
 
+// TODO: NData should probably be a union of things you can eval, things you can
+// apply, and things you can macro apply. There should be no data that
+// implements an interface method with a straight up throw.
 export interface NData {
     evaluate(environment: Environment): Process
     apply(args: Array<NData>): Process
